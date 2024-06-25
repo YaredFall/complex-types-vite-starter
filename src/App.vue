cded925d0a3718f5e41ef0444c50a2513abce603
YaredFall <yaredfall@gmail.com>
2024-06-25 10:17:33 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import CvButton from './components/cv-button.vue';
+
+</script>
+
+<template>
+  <CvButton>Vite + Vue</CvButton>
+</template>
---
+++ src/components/cv-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    import { ButtonHTMLAttributes } from "vue";
+
+    interface MyButtonProps extends ButtonHTMLAttributes {}
+
+    defineProps<MyButtonProps>();
+</script>
+
+<template>
+    <button><slot /></button>
+</template>
